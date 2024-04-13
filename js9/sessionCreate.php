4fe36e4dcf5feded814d701c369effb0a527fc5c
prihandiva <prihandiva1711@gmail.com>
2024-04-13 22:08:16 +0700

    Pertemuan 9

---
+++ js9/sessionCreate.php
@@ -0,0 +1,14 @@
+
+<?php
+    session_start();
+?>
+
+<html>
+    <body>
+        <?php
+            $_SESSION["favcolor"] = "green";
+            $_SESSION["favanimal"] = "set";
+            echo "Session variables are set.";
+        ?> 
+    </body>
+</html>
