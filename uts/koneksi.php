5884a77055137ae13f202bbfcdee7f686406f563
prihandiva <prihandiva1711@gmail.com>
2024-04-19 05:54:09 +0700

    UTS

--- uts/koneksi.php
+++ uts/koneksi.php
@@ -1,24 +1,15 @@
 <?php
-include "koneksi.php";
+// Informasi koneksi ke database
+$host = "localhost"; // Host database (biasanya "localhost")
+$username = "username_database"; // Username database
+$password = "password_database"; // Password database
+$database = "uts_web"; // Nama database
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+// Membuat koneksi ke database
+$connect = mysqli_connect($host, $username, $password, $database);
 
-$query = "SELECT * FROM mahasiswa WHERE username='$username' AND password='$password'";
-try {
-    $result = mysqli_query($connect, $query);
-    $cek = mysqli_num_rows($result);
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
-
-if ($cek) {
-    echo "Anda berhasil login. Silahkan menuju "; ?>
-        <a href = "index.html">Halaman Ujian</a>
-        <?php
-} else {
-    echo "Anda gagal login. Silahkan coba lagi"; ?>
-        <a href = "login.html"> Login Kembali</a>
-        <?php echo mysqli_error($connect);
+// Memeriksa koneksi
+if (!$connect) {
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
 ?>
